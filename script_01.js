39abacd8cd249a9bbf02cb16d6e51f1c655248b9
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-17 12:43:32 +0100

    Objekte 1 Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -1,11 +1,12 @@
 
 /***** Erinnerung: Arrays *******/
 
-// let arr;
-// arr = ["Ich","bin","Max"];
-// output(arr);
-// output(arr[0]);
-// output("------------");
+// default arrays
+    // let arr;
+    // arr = ["Ich","bin","Max"];
+    // output(arr);
+    // output(arr[0]);
+    // output("------------");
 
 // nested arrays
     // arr = [
@@ -18,17 +19,34 @@
     // output("------------");
     
 // nested loops
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[0].length; j++) { 
-            output(arr[i][j]);          
-           }   
+    // for (let i = 0; i < arr.length; i++) {
+    //     for (let j = 0; j < arr[0].length; j++) { 
+    //         output(arr[i][j]);          
+    //        }   
+    // }
+    
+    
+/*******   Objekte 1 Daten/Funktionen   *******/
+    // Modul 1: Objekt erstellen
+    let person = {
+        firstName: "Max",
+        lastName: "Mustermann",
+        sallary: [ 120000, 160000 ],
+        doorPermission: false,
+        sayHello: function() { 
+            return "Hallo ich bin " + this.firstName + " " + this.lastName ;
+         }
     }
-    
-/********************************************************/
-/*******                Example 01                *******/
-/********************************************************/
-    
-/***** Objekte 1 Daten/Funktionen *******/
+    // Modul 1: Ausgabe Test
+    // output( person );
+    // output( person.firstName );
+    // output( person.lastName );
+    // output( person.sallary );
+
+    // Modul 2: Objekt verschachteln und verknüpfen
+    const testText = "Ich bin " + person.firstName + " " + person.lastName + " und verdiene " + person.sallary[ 1 ].toLocaleString('de-DE') + " € im Jahr.";
+    output( testText );
+    output( person.sayHello );
     
     
     
