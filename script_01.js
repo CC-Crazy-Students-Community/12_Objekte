dc652eb5bc39dd2acf1499fe5a3a02c7d3951780
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-17 12:18:53 +0100

    Erinnerung: Arrays

--- script_01.js
+++ script_01.js
@@ -7,28 +7,33 @@
 // output(arr[0]);
 // output("------------");
 
-// // nested arrays
-// arr =   [
-//                 ["Ich","bin","Max"],
-//                 ["Ich","bin","Moritz"]
-//         ];
-
-// output(arr[0][2]);
-// output(arr[1][2]);
-// output("------------");
-
-// // nested loops
-// for (let i = 0; i < arr.length; i++) {
-//        for (let j = 0; j < arr[0].length; j++) { 
-//         output(arr[i][j]);          
-//        }   
-// }
-
+// nested arrays
+    // arr = [
+    //     ["Ich","bin","Max"],
+    //     ["Ich","bin","Moritz"]
+    // ];
+    
+    // output(arr[0][2]);
+    // output(arr[1][2]);
+    // output("------------");
+    
+// nested loops
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) { 
+            output(arr[i][j]);          
+           }   
+    }
+    
+/********************************************************/
+/*******                Example 01                *******/
+/********************************************************/
+    
 /***** Objekte 1 Daten/Funktionen *******/
-
-
-
-/** Ausgabe */
-function output(outputData) {
-        console.log(outputData);
-}
+    
+    
+    
+/********************************************************/
+/*******                  Output                  *******/
+/********************************************************/
+    // output
+    function output( outputStr ) { console.log( outputStr ); }
